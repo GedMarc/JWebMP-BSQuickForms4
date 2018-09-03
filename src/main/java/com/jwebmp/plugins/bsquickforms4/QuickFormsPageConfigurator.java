84f3b2e3e39895114746b1cd25e80208f4726c7b
GedMarc <ged_marc@hotmail.com>
2018-09-03 22:51:08 +0200

    Make page configuration optional, update the app

--- src/main/java/com/jwebmp/plugins/bsquickforms4/QuickFormsPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/bsquickforms4/QuickFormsPageConfigurator.java
@@ -35,12 +35,42 @@ import javax.validation.constraints.NotNull;
 public class QuickFormsPageConfigurator
 		implements IPageConfigurator
 {
+	/**
+	 * If this configurator is enabled
+	 */
+	private static boolean enabled = true;
+
 	/*
 	 * Constructs a new QuickFormsPageConfigurator
 	 */
 	public QuickFormsPageConfigurator()
 	{
 		//Nothing needed
+	}
+
+	/**
+	 * Method isEnabled returns the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @return the enabled (type boolean) of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static boolean isEnabled()
+	{
+		return QuickFormsPageConfigurator.enabled;
+	}
+
+	/**
+	 * Method setEnabled sets the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @param mustEnable
+	 * 		the enabled of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static void setEnabled(boolean mustEnable)
+	{
+		QuickFormsPageConfigurator.enabled = mustEnable;
 	}
 
 	@NotNull
@@ -52,5 +82,11 @@ public class QuickFormsPageConfigurator
 			AngularPageConfigurator.setRequired(true);
 		}
 		return page;
+	}
+
+	@Override
+	public boolean enabled()
+	{
+		return QuickFormsPageConfigurator.enabled;
 	}
 }
