e52910d2edbb1dd8e3edda8f6c21ffd66241029d
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:35:43 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -4,7 +4,7 @@ module com.jwebmp.plugins.bs4.quickforms {
 	exports com.jwebmp.plugins.bs4.quickforms.annotations.actions;
 
 	requires com.jwebmp.core;
-	requires com.jwebmp.logmaster;
+	requires com.guicedee.logmaster;
 	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
@@ -13,7 +13,7 @@ module com.jwebmp.plugins.bs4.quickforms {
 	requires com.jwebmp.plugins.quickforms;
 	requires com.jwebmp.plugins.bs4.datetimepicker;
 	requires com.jwebmp.plugins.bs4.toggle;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 
 	requires java.logging;
 	requires org.apache.commons.text;
@@ -21,8 +21,8 @@ module com.jwebmp.plugins.bs4.quickforms {
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.bs4.quickforms.BSQuickFormsPageConfigurator;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.bs4.quickforms.implementations.BSQuickForms4ExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.bs4.quickforms.implementations.BSQuickForms4ExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.bs4.quickforms.implementations.BSQuickForms4ExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.bs4.quickforms.implementations.BSQuickForms4ExclusionsModule;
 
 	opens com.jwebmp.plugins.bs4.quickforms.annotations.actions to com.fasterxml.jackson.databind, com.jwebmp.core;
 	opens com.jwebmp.plugins.bs4.quickforms.annotations.implementations to com.fasterxml.jackson.databind, com.jwebmp.core;
