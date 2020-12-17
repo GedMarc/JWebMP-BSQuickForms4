30e4ff2081f66d9ce280052d05ae574934bfdf82
GedMarc <ged_marc@hotmail.com>
2020-12-17 09:03:02 +0200

    Pge configurator type safety and test class updates for it

--- src/main/java/com/jwebmp/plugins/bs4/quickforms/BSQuickFormsPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/bs4/quickforms/BSQuickFormsPageConfigurator.java
@@ -97,7 +97,7 @@ public class BSQuickFormsPageConfigurator
 
 	@NotNull
 	@Override
-	public Page configure(Page page)
+	 public Page<?> configure(Page<?> page)
 	{
 		if (!page.isConfigured() && enabled())
 		{
